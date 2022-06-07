bb7602b277922b6e7cd5ebd2b20515ad38c03433
giovannav <giovannafrederico60@gmail.com>
2022-06-07 11:48:22 -0300

    atualizacao

--- cliente_1.c
+++ cliente_1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdio_ext.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,13 +12,27 @@
 
 #define BUFFER_SIZE 1024
 
+void welcome(){
+    printf("******************************");
+    printf("\nBem vindo ao cliente_1.c\n");
+    printf("\nCertifique-se que o cliente 2 também está online.\n");
+    printf("\nPressione qualquer tecla para continuar.\n");
+    printf("******************************");
+}
+
 int main(void)
 {
+
+    welcome();
+    getchar();
+
     struct sockaddr_in sock;
     int con, sockid;
     int port = 5002;
-    char buffer[BUFFER_SIZE];
-    char get_concat[500];
+    char buffer_recv[BUFFER_SIZE];
+    char buffer_send[BUFFER_SIZE];
+    char get_ip[20];
+    char get_port[20];
     struct in_addr addr;
 
     sockid = socket(AF_INET, SOCK_STREAM, 0); //criando o socket
@@ -31,27 +46,35 @@ int main(void)
 
     if (con < 0){
         printf("\nErro ao conectar\n");
+        close(sockid);
     }
-    else{
-        printf("\nConectado\n");
-    }
-    bzero(buffer, BUFFER_SIZE);
+    
 
-    bzero(buffer, BUFFER_SIZE);
-    while(recv(sockid, buffer, BUFFER_SIZE-1, 0) != 0)
-    {
-        printf("%s", buffer);
-        if(!strcmp(buffer, "Digite a hora:\n\n"))
+    while(recv(sockid, buffer_recv, BUFFER_SIZE, 0) != 0)
+    {   
+        printf("Buffer antes: %s\n", buffer_recv);
+        if(memcmp(buffer_recv, "\n*******\nDigite o ip com que deseja se conectar:\n*******\n", strlen("\n*******\nDigite o ip com que deseja se conectar:\n*******\n")) == 0)
         {
-            //printf("%s", buffer);
-            bzero(buffer, BUFFER_SIZE);
-            scanf("%s", buffer);
-            send(sockid, buffer, strlen(buffer), 0);
-            bzero(buffer, BUFFER_SIZE);
-            printf("%s", buffer);
-           // recv(sockid, buffer, BUFFER_SIZE-1, 0);
+            printf("\nEntrou memc\n");
+            scanf("%s", get_ip);
+            __fpurge(stdin);
+            send(sockid, get_ip, strlen(get_ip), 0);
+            
         }
         
+        printf("Antes da compraçao\n");
+        printf("\nBuffer %s\n",buffer_recv);
+        if (!strcmp(buffer_recv,"\nDigite a porta do cliente 2:\n"))
+        //else if(memcmp(buffer_recv, "\nDigite a porta do cliente 2:\n\0", strlen("\nDigite a porta do cliente 2:\n\0")) == 0)
+        {printf("\nentrou no str\n");
+            printf("%s", buffer_recv);
+            scanf("%s", get_port);
+            __fpurge(stdin);
+            send(sockid, get_port, strlen(get_port), 0);
+            bzero(buffer_recv, sizeof(buffer_recv));
+        }
+        printf("\ndepois\n");
+        bzero(buffer_recv, sizeof(buffer_recv));
     }
     close(sockid);
 }
--- exe_1_servidor.c
+++ exe_1_servidor.c
@@ -11,7 +11,7 @@
 
 #define BUFFER_SIZE 1024
 
-int connect_client2()
+int connect_client2(char *ip,  char *port)
 {
     //nc -l -p 5000
     struct sockaddr_in sock;
@@ -19,8 +19,8 @@ int connect_client2()
     char buffer[BUFFER_SIZE];
     sockid = socket(AF_INET, SOCK_STREAM, 0);
     sock.sin_family = AF_INET;
-    sock.sin_port = htons(5000);
-    inet_pton(AF_INET, "127.0.0.1", &sock.sin_addr);
+    sock.sin_port = htons(atoi(port));
+    inet_pton(AF_INET, ip, &sock.sin_addr);
 
     con = connect(sockid, (struct sockaddr *)&sock, sizeof(sock));
     if (con < 0)
@@ -96,10 +96,60 @@ int main(void)
         }
         if (pid == 0)
         {
+            printf("\nProcesso filho criado\n");
+
+            // ------------------------ Mensagem de boas vindas ao cliente 1
             close(sockid);
-            send(newSocket_1, "Olá, eu sou o servidor\n", strlen("Olá, eu sou o servidor\n"), 0);
-            connect_client2();
-            close(newSocket_1);
+            char welcome[100] = "\n************************\nMsg do servidor: Cliente 1 conectado\n*************************\n";
+            send(newSocket_1, welcome, strlen(welcome), 0);
+          //  bzero(&welcome, sizeof(welcome));
+            //welcome[0] = '\0';
+            // ----------------------------------------------------------------------------------
+
+            // ------------------------ Recebe o IP do cliente 2 e envia de volta ao cliente 1
+            char connect1[100] = "\n*******\nDigite o ip com que deseja se conectar:\n*******\n";
+            send(newSocket_1, connect1, strlen(connect1), 0);
+           // bzero(&connect1, sizeof(connect1));
+            connect1[0] = '\0';
+            char get_ip[20];
+            char get_concat[500];
+            get_concat[0] = '\0';
+           // bzero(&get_ip, sizeof(get_ip));
+            recv(newSocket_1, get_ip, sizeof(get_ip), 0);
+            snprintf(get_concat, sizeof(get_concat), "\n\n****Msg do servidor: endereço recebido -> %s\n", get_ip);
+            send(newSocket_1, get_concat, strlen(get_concat), 0);
+            get_concat[0] = '\0';
+         //  bzero(&get_concat, sizeof(get_concat));
+            int control = 1;
+            printf("\n------> Checkpoint 1\n");
+            
+            //  ---------------------------------------------------------------------------------
+
+            // ------------------------ Recebe a porta do cliente 2 e envia de volta ao cliente 1
+            if(control == 1);
+            {
+                printf("\n -----> Checkpoint 2\n");
+                char connect2[100] = "\nDigite a porta do cliente 2:\n";
+                printf("\n -----> Checkpoint 3\n");
+                printf("connect2 %s", connect2);
+                send(newSocket_1, connect2, strlen(connect2), 0);
+                bzero(connect2, sizeof(connect2));
+                printf("\n -----> Checkpoint 4\n");
+                char get_port[20];
+                bzero(get_port, sizeof(get_port));
+                bzero(get_concat, sizeof(get_concat));
+                recv(newSocket_1, get_port, sizeof(get_port), 0);
+                snprintf(get_concat, sizeof(get_concat), "\n\n****Msg do servidor: porta recebida -> %s\n", get_port);
+                send(newSocket_1, get_concat, strlen(get_concat), 0);
+                bzero(get_concat, sizeof(get_concat));
+                connect_client2(get_ip, get_port);
+            }
+            
+            //  ---------------------------------------------------------------------------------
+
+            
+
+            //close(newSocket_1);
             exit(0);
         } // if do filho
         else
